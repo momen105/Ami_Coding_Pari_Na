bfa6d4b4a0ff8a21a71b872f4a639a9fbed638e8
momen105 <momentohin02@gmail.com>
2022-11-10 01:54:25 +0600

    allowed host

--- src/core/settings.py
+++ src/core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = "django-insecure-3k59gj5!=d0sqd(-6&jrcd53e%g(wp3$3s77+#wkhk08sf001-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
